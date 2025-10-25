672d8cba1f33aeeca62004a8c9a527e617083535
Kavia AI <production@kavia.ai>
2025-10-25 09:16:00 +0000

    Auto-commit: session cg9f5c3cb4 - tic-tac-toe-web-application-214883-214978

--- tic_tac_toe_database/database_backup.sql
+++ tic_tac_toe_database/database_backup.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
-\restrict 9OKEgtSNLMzB3OHZ9jOLlHrM0WGC8MnqwDA8eeCTDIq35fTXv2lFiULc8QqNJ6D
+\restrict CkqXa9pietUXSsh3GA7tg9uGTVq1rvNMIvZWefcIPiPOw2llzVrhBPeIqCpbgI6
 
 -- Dumped from database version 16.10 (Ubuntu 16.10-0ubuntu0.24.04.1)
 -- Dumped by pg_dump version 16.10 (Ubuntu 16.10-0ubuntu0.24.04.1)
@@ -28,9 +28,9 @@ CREATE DATABASE myapp WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE_PROVIDE
 
 ALTER DATABASE myapp OWNER TO postgres;
 
-\unrestrict 9OKEgtSNLMzB3OHZ9jOLlHrM0WGC8MnqwDA8eeCTDIq35fTXv2lFiULc8QqNJ6D
+\unrestrict CkqXa9pietUXSsh3GA7tg9uGTVq1rvNMIvZWefcIPiPOw2llzVrhBPeIqCpbgI6
 \connect myapp
-\restrict 9OKEgtSNLMzB3OHZ9jOLlHrM0WGC8MnqwDA8eeCTDIq35fTXv2lFiULc8QqNJ6D
+\restrict CkqXa9pietUXSsh3GA7tg9uGTVq1rvNMIvZWefcIPiPOw2llzVrhBPeIqCpbgI6
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -89,5 +89,5 @@ ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA public GRANT ALL ON TABLES 
 -- PostgreSQL database dump complete
 --
 
-\unrestrict 9OKEgtSNLMzB3OHZ9jOLlHrM0WGC8MnqwDA8eeCTDIq35fTXv2lFiULc8QqNJ6D
+\unrestrict CkqXa9pietUXSsh3GA7tg9uGTVq1rvNMIvZWefcIPiPOw2llzVrhBPeIqCpbgI6
 
